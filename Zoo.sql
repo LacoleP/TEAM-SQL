4137905d456f165b4bcc27741f573f0c5fc690b3
bigbedda <stuartjamesbedford@hotmail.com>
2022-01-28 15:05:45 +0000

    SB; added zoo section

--- Zoo.sql
+++ Zoo.sql
@@ -1,3 +1,10 @@
 CREATE DATABASE Zoo;
 
 USE Zoo;
+
+CREATE TABLE zoo (
+staff_names VARCHAR(30),
+staff_id INT AUTO_INCREMENT PRIMARY KEY,
+staff_contact_number INT UNIQUE,
+staff_animal_speciality VARCHAR(30)
+
