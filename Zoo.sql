3bdba9de2b985fb992da8b01990a28cda17a4f34
MohammadAl-Fartousy <Mohammed_al-fartousy@hotmail.com>
2022-01-28 14:28:41 +0000

    hi

--- Zoo.sql
+++ Zoo.sql
@@ -1,3 +1,14 @@
 CREATE DATABASE Zoo;
 
 USE Zoo;
+CREATE TABLE animals (
+lion VARCHAR(40),
+tiger VARCHAR(40),
+shark VARCHAR(40),
+whale VARCHAR(90),
+snake VARCHAR(60),
+bear VARCHAR(70),
+crocodile VARCHAR(90),
+peacock VARCHAR(60));
+
+DESCRIBE animals;
