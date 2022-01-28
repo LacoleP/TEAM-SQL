0d62e3bdca2e3cfaa000f1482d4fe70f03eba3e7
LacoleP <97948277+LacoleP@users.noreply.github.com>
2022-01-28 15:16:16 +0000

    Update Zoo.sql

--- Zoo.sql
+++ Zoo.sql
@@ -1,15 +1,7 @@
 CREATE DATABASE Zoo;
 
 USE Zoo;
-<<<<<<< HEAD
 
-CREATE TABLE zoo (
-staff_names VARCHAR(30),
-staff_id INT AUTO_INCREMENT PRIMARY KEY,
-staff_contact_number INT UNIQUE,
-staff_animal_speciality VARCHAR(30)
-
-=======
 CREATE TABLE animals (
 lion VARCHAR(40),
 tiger VARCHAR(40),
@@ -21,4 +13,10 @@ crocodile VARCHAR(90),
 peacock VARCHAR(60));
 
 DESCRIBE animals;
->>>>>>> 3bdba9de2b985fb992da8b01990a28cda17a4f34
+  
+  CREATE TABLE zoo (
+staff_names VARCHAR(30),
+staff_id INT AUTO_INCREMENT PRIMARY KEY,
+staff_contact_number INT UNIQUE,
+staff_animal_speciality VARCHAR(30)
+
